2230dd77b5ba86b4f8fcc62adbafdcb79f95f26c
nya.dvlp@gmail.com <nya.dvlp@gmail.com>
2021-08-27 13:53:27 +0900

    웹개발 기초 - api까지 완료

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,13 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello (Model model) {
-        model.addAttribute("data", "datahello");
+        model.addAttribute("data", "this is data");
         return "hello";
     }
 
@@ -19,4 +20,73 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @GetMapping("practice-api")
+    @ResponseBody
+    public Practice practiceApi(@RequestParam("irum") String name,
+                                @RequestParam("nai") int age,
+                                @RequestParam("juso") String address) {
+        Practice practice = new Practice();
+        practice.setName(name);
+        practice.setAge(age);
+        practice.setAddress(address);
+        return practice;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    static class Practice {
+        private String name;
+        private int age;
+        private String address;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+    }
 }
+
