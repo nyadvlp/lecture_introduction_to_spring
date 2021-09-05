5242a5481bb99843c7f5dda2c45a1fad09541b3b
nyadvlp <nya.dvlp@gmail.com>
2021-09-05 22:35:01 +0900

    웹 MVC 개발 - 회원 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -22,11 +25,18 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
-    @PostMapping ("/members/new")
+    @PostMapping("/members/new")
     public String create(MemberForm form) {
         Member member = new Member(); // CRUD 작업을 위한 객체 생성
         member.setName(form.getName()); // form으로 받아온 이름 세팅
         memberService.join(member); // 만들어 둔 create 작업
         return "redirect:/"; // home 화면으로 돌려보냄
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
