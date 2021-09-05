7e39a1757ab65a144b10096b0fdd6f5be557608b
nyadvlp <nya.dvlp@gmail.com>
2021-09-05 22:26:53 +0900

    웹 MVC 개발 - 홈화면 추가, 회원 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,20 +1,32 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
 
     private final MemberService memberService;
 
-    // BAD : 불필요하게 여러 개의 객체가 생성됨
-    // private final MemberService memberService = new MemberService();
-
-    // GOOD : 컨테이너에 등록하여 활용
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping ("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member(); // CRUD 작업을 위한 객체 생성
+        member.setName(form.getName()); // form으로 받아온 이름 세팅
+        memberService.join(member); // 만들어 둔 create 작업
+        return "redirect:/"; // home 화면으로 돌려보냄
     }
 }
